ea9ceb8e7c0379a499998d5dc18a7273d70f2324
deejayres <erinresso@gmail.com>
2016-01-31 15:48:18 -0800

    complete assignment 25: test and create faq

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -8,4 +8,20 @@ RSpec.describe WelcomeController, type: :controller do
       expect(response).to render_template("index")
     end
   end
+
+  describe "GET about" do
+    it "renders the about template" do
+      get :about
+
+      expect(response).to render_template("about")
+    end
+  end
+
+  describe "GET FAQ" do
+    it "renders the FAQ template" do
+      get :faq
+
+      expect(response).to render_template("faq")
+    end
+  end
 end
