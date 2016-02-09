6f34c4f12efd8cf7e080aa42e97e9ca00c9290be
deejayres <erinresso@gmail.com>
2016-02-09 15:46:27 -0800

    complete assignment 33, implement edit and delete, add resolved chckbox

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -25,4 +25,35 @@ class QuestionsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.title = params[:question][:title]
+    @question.body = params[:question][:body]
+    @question.resolved = params[:question][:resolved]
+
+    if @question.save
+      flash[:notice] = "Question was updated"
+      redirect_to @question
+    else
+      flash.new[:alert] = "There was an error saving the question. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @question = Question.find(params[:id])
+
+    if @question.destroy
+      flash[:notice] = "\"#{@question.title}\" was deleted successfully."
+      redirect_to questions_path
+    else
+      flash.now[:alert] = "There was an error deleting the post."
+      render :show
+    end
+  end
 end
--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -58,13 +58,6 @@ RSpec.describe QuestionsController, type: :controller do
     end
   end
 
-  describe "GET #create" do
-    it "returns http success" do
-      get :create
-      expect(response).to have_http_status(:success)
-    end
-  end
-
   describe "POST #create" do
 
     it "increases the number of Question by 1" do
@@ -81,6 +74,74 @@ RSpec.describe QuestionsController, type: :controller do
       post :create, question: {title: RandomData.random_sentence, body: RandomData.random_paragraph, resolved: false }
 
       expect(response).to redirect_to Question.last
+    end
+  end
+
+  describe "GET #edit" do
+
+    it "returns HTTP success" do
+      get :edit, {id: my_question.id}
+
+      expect(response). to have_http_status(:success)
+    end
+
+    it "renders the edit view" do
+      get :edit, {id: my_question.id}
+
+      expect(response).to render_template(:edit)
+    end
+
+    it "assigns question to be updated to @question" do
+      get :edit, {id: my_question.id}
+      question_instance = assigns(:question)
+
+      expect(question_instance.id).to eq my_question.id
+      expect(question_instance.title).to eq my_question.title
+      expect(question_instance.body).to eq my_question.body
+      expect(question_instance.resolved).to eq my_question.resolved
+    end
+  end
+
+  describe "PUT #update" do
+
+    it "updates post with expected attributes" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_resolved = true
+
+      put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+      updated_question = assigns(:question)
+
+      expect(updated_question.id).to eq(my_question.id)
+      expect(updated_question.title).to eq(new_title)
+      expect(updated_question.body).to eq(new_body)
+      expect(updated_question.resolved).to eq(new_resolved)
+    end
+
+    it "redirects to the updated post" do
+      new_title = RandomData.random_sentence
+      new_body = RandomData.random_paragraph
+      new_resolved = true
+
+      put :update, id: my_question.id, question: {title: new_title, body: new_body, resolved: new_resolved}
+
+      expect(response).to redirect_to(my_question)
+    end
+  end
+
+  describe "DELETE #destroy" do
+
+    it "deletes the question" do
+      delete :destroy, {id: my_question.id}
+      count = Question.where({id: my_question.id}).size
+
+      expect(count).to eq(0)
+    end
+
+    it "redirects to the questions index" do
+      delete :destroy, {id: my_question.id}
+
+      expect(response).to redirect_to questions_path
     end
   end
 
