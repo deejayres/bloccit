ba499cab120c5733af2cbb3cc8f29287f7fb06a9
deejayres <erinresso@gmail.com>
2016-02-09 22:17:04 -0800

    mid checkpoint 34, implement new, create, edit for Topic

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -7,4 +7,26 @@ class TopicsController < ApplicationController
   def show
     @topic = Topic.find(params[:id])
   end
+
+  def new
+    @topic = Topic.new
+  end
+
+  def create
+    @topic = Topic.new
+    @topic.name = params[:topic][:name]
+    @topic.description = params[:topic][:description]
+    @topic.public = params[:topic][:public]
+
+    if @topic.save
+      redirect_to @topic, notice: "Topic was saved successfully."
+    else
+      flash.now[:alert] = "Error creating topic. Please try again."
+      render :new
+    end
+  end
+
+  def edit
+    @topic = Topic.find(params[:id])
+  end
 end
--- spec/controllers/topics_controller_spec.rb
+++ spec/controllers/topics_controller_spec.rb
@@ -38,4 +38,65 @@ RSpec.describe TopicsController, type: :controller do
     end
   end
 
+  describe "GET new" do
+    it "returns HTTP success" do
+      get :new
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders :new" do
+      get :new
+
+      expect(response).to render_template :new
+    end
+
+    it "initializes @topic" do
+      get :new
+
+      expect(assigns(:topic)).not_to be_nil
+    end
+  end
+
+  describe "POST create" do
+    it "increases the number of topics by 1" do
+      expect{ post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}}.to change(Topic, :count).by(1)
+    end
+
+    it "assigns Topic.last to @topic" do
+      post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
+
+      expect(assigns(:topic)).to eq(Topic.last)
+    end
+
+    it "redirects to the new topic" do
+      post :create, {topic: {name: RandomData.random_sentence, description: RandomData.random_paragraph}}
+
+      expect(response).to redirect_to Topic.last
+    end
+  end
+
+  describe "GET edit" do
+    it "returns HTTP success" do
+      get :edit, { id: my_topic.id }
+
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #edit view" do
+      get :edit, { id: my_topic.id }
+
+      expect(response).to render_template :edit
+    end
+
+    it "assigns topic to be updated to @topic" do
+      get :edit, { id: my_topic.id }
+      topic_instance = assigns(:topic)
+
+      expect(topic_instance.id).to eq(my_topic.id)
+      expect(topic_instance.name).to eq(my_topic.name)
+      expect(topic_instance.description).to eq(my_topic.description)
+    end
+  end
+
 end
