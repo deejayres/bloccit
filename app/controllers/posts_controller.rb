78314769bc9445880c6fb4600d8d1d32d2a9d445
deejayres <erinresso@gmail.com>
2016-02-12 20:16:12 -0600

    complete checkpoint 35, add post validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,7 +20,7 @@ class PostsController < ApplicationController
       flash[:notice] = "Post was saved."
       redirect_to [@topic, @post]
     else
-      flash.new[:alert] = "There was an error saving the post. Please try again."
+      flash.now[:alert] = "There was an error saving the post. Please try again."
       render :new
     end
   end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -5,7 +5,14 @@ RSpec.describe Post, type: :model do
   let(:topic) { Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph) }
   let(:post) { topic.posts.create!(title: RandomData.random_sentence, body: RandomData.random_paragraph) }
   it { is_expected.to belong_to(:topic) }
-  
+
+  it { is_expected.to validate_presence_of(:title) }
+  it { is_expected.to validate_presence_of(:body) }
+  it { is_expected.to validate_presence_of(:topic) }
+
+  it { is_expected.to validate_length_of(:title).is_at_least(5) }
+  it { is_expected.to validate_length_of(:body).is_at_least(20) } 
+
   describe "attributes" do
 
     it "responds to Title" do
