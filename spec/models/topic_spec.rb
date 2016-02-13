077b0be2d9a8b0f0c8e9df715d6c09b23f6328aa
deejayres <erinresso@gmail.com>
2016-02-12 23:11:41 -0600

    complete assignment 35, validate topics

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -4,6 +4,13 @@ RSpec.describe Topic, type: :model do
 
   let (:topic) {Topic.create!(name: RandomData.random_sentence, description: RandomData.random_paragraph)}
   it {is_expected.to have_many(:posts) }
+
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:description) }
+
+  it { is_expected.to validate_length_of(:name).is_at_least(5) }
+  it { is_expected.to validate_length_of(:description).is_at_least(15) }
+
   describe "topic responds to attributes" do
     it "responds to name" do
       expect(topic).to respond_to(:name)
