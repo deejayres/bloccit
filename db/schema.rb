8b0facaa70377597aa39351491a0ccda70b02ef2
deejayres <erinresso@gmail.com>
2016-02-14 19:33:51 -0600

    complete checkpoint 36, create user resource

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210003528) do
+ActiveRecord::Schema.define(version: 20160215005642) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -40,12 +40,31 @@ ActiveRecord::Schema.define(version: 20160210003528) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "sponsered_posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.integer  "price"
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "sponsered_posts", ["topic_id"], name: "index_sponsered_posts_on_topic_id"
+
   create_table "topics", force: :cascade do |t|
     t.string   "name"
     t.boolean  "public",      default: true
     t.text     "description"
     t.datetime "created_at",                 null: false
     t.datetime "updated_at",                 null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "password_digest"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
   end
 
 end
