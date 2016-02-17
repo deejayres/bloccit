2b28f1c51bba21fb5e419808b0a4e1d6b8d6f94e
deejayres <erinresso@gmail.com>
2016-02-16 17:58:41 -0800

    complete checkpoint 39, associate users with posts, limit actions to signed-in users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215005642) do
+ActiveRecord::Schema.define(version: 20160217003419) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -28,9 +28,11 @@ ActiveRecord::Schema.define(version: 20160215005642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
+    t.integer  "user_id"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
+  add_index "posts", ["user_id"], name: "index_posts_on_user_id"
 
   create_table "questions", force: :cascade do |t|
     t.string   "title"
@@ -39,17 +41,6 @@ ActiveRecord::Schema.define(version: 20160215005642) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-  create_table "sponsered_posts", force: :cascade do |t|
-    t.string   "title"
-    t.text     "body"
-    t.integer  "price"
-    t.integer  "topic_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  add_index "sponsered_posts", ["topic_id"], name: "index_sponsered_posts_on_topic_id"
 
   create_table "topics", force: :cascade do |t|
     t.string   "name"
