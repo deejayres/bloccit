c15dfc7c2cf57d6751964a36cfd5832b12f5530d
deejayres <erinresso@gmail.com>
2016-02-26 11:49:10 -0800

    complete assignment-46, implement publicly and privately viewable scopes

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,5 +3,8 @@ class Topic < ActiveRecord::Base
   has_many :labelings, as: :labelable
   has_many :labels, through: :labelings
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) { user ? all : Topic.publicly_viewable }
+  scope :publicly_viewable, -> { where(public: true) }
+  scope :privately_viewable, -> { where(public: false) }
+
 end
--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -42,6 +42,18 @@ RSpec.describe Topic, type: :model do
         expect(Topic.visible_to(nil)).to eq([@public_topic])
       end
     end
+
+    describe "publicly_viewable" do
+      it "returns all public topics" do
+        expect(Topic.publicly_viewable).to eq(Topic.where(public: true))
+      end
+    end
+
+    describe "privately_viewable" do
+      it "returns all private topics" do
+        expect(Topic.privately_viewable).to eq(Topic.where(public: false))
+      end
+    end
   end
 
 end
